e736b1d4ffd42ad6f307963c7a29a878ddd842af
Arckanna <valerie.lecoeur75@gmail.com>
2019-08-09 19:09:19 +0200

    modif extension image

--- monscript.js
+++ monscript.js
@@ -1,9 +1,9 @@
 
 function bigImg(x){
   var b = x;
-  var arr = { "HTML" : "html.jpg", "Javascript" : "javascript.jpg", "CSS": "css.jpg" ,
-              "jquery" :"jquery.jpg","php":"php.jpg","sql":"sql.jpg","java":"java.jpg",
-              "c":"c.jpg","python":"python.jpg"};
+  var arr = { "HTML" : "html.webp", "Javascript" : "javascript.webp", "CSS": "css.webp" ,
+              "jquery" :"jquery.webp","php":"php.webp","sql":"sql.webp","java":"java.webp",
+              "c":"c.webp","python":"python.webp"};
   var res = "<img src='img/"+arr[b]+"' alt='valerie lecoeur' style='max-height: 400px;'>";
 
   $('.modal-body').append(res);
